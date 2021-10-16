03b5be65d449fe2339f23ca899bb3b139ec2c0f6
victorsnunes <victorsnunes99@gmail.com>
2021-10-16 20:08:10 +0100

    Embaralhando as cartas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,6 +14,29 @@ let techs = [
     'react'
 ];
 
+let cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+
+    console.log(cards);
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
+    }
+}
+
 function createCardsFromTechs(techs) {
 
     let cards = [];
