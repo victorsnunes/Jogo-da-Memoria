c7ea9fa4e7425dd39d688bd53cd38724f51d248c
victorsnunes <victorsnunes99@gmail.com>
2021-10-17 17:45:22 +0100

    separacao view e logica

---
+++ assets/scripts/game.js
@@ -0,0 +1,60 @@
+let game = {
+
+    techs: [
+        'bootstrap',
+        'css',
+        'electron',
+        'firebase',
+        'html',
+        'javascript',
+        'jquery',
+        'mongo',
+        'node',
+        'react'
+    ],
+
+    cards: null,
+
+    createCardsFromTechs: function () {
+
+        this.cards = [];
+
+        for (let tech of this.techs) {
+            this.cards.push(this.createPairFromTech(tech));
+        }
+
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
+
+        return this.cards;
+    },
+
+    createPairFromTech: function (tech) {
+        return [{
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false
+        },
+        {
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false
+        }]
+    },
+
+    createIdWithTech: function (tech) {
+        return tech + parseInt(Math.random() * 1000);
+    },
+
+    shuffleCards: function () {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
+        }
+    }
+};
